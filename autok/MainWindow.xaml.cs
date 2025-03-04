a1a75472f70211fdfc6ef36af58a9a3297420464
Ballam906 <ballam@kkszki.hu>
2025-03-04 19:29:20 +0100

    1.feladat

--- autok/MainWindow.xaml.cs
+++ autok/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,23 @@ namespace autok
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<auto> lista = new List<auto>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void beolvas()
+        {
+            using (var sr = new StreamReader("jeladas.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var sor = sr.ReadLine().Split('\t');
+                    var krater = new auto(sor[0], int.Parse(sor[1]), int.Parse(sor[2]), int.Parse(sor[3]));
+                    lista.Add(krater);
+                }
+            }
         }
     }
 }
---
+++ autok/auto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autok
+{
+    internal class auto
+    {
+        public string rendszam { get; set; }
+        public int ora { get; set; }
+        public int perc { get; set; }
+        public int sebesseg { get; set; }
+
+        public auto(string rend,int or,int pe,int seb)
+        {
+            rendszam=rend;
+            ora=or; 
+            perc=pe;
+            sebesseg=seb;
+        }
+    }
+}
